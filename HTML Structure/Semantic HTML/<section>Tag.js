2a7b6df79e5778fdf55cd948cf3782a031283331
MariyanMaksimov <mariyan.pickup@gmail.com>
2020-07-08 16:33:50 +0200

    Create <section>Tag.js

---
+++ HTML Structure/Semantic HTML/<section>Tag.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Semantic tags -> Section</title>
+</head>
+
+<body>
+
+    <main>
+        <li>Section tag represents a standalone section, usually followed by a heading.</li>
+    </main>
+
+    <section>
+        <h1>Heading</h1>
+        <img src="bird.jpg"/>
+    </section>
+    
+    <footer>
+        <p>Made by: Mariyan</p>
+        <p><a href="mariyan.pickup@gmail.com">Drop a line..</a></p>
+        <p>&copy;2020-2021</p>
+    </footer>
+
+</body>
+
+</html>
